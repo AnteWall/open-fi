fa6e46294496e4c7e7e1b4537fbd4f5051fd2e53
Ante Wall <antewall@gmail.com>
2021-11-11 16:37:32 +0100

    add rest and swagger docs

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,21 +1,33 @@
+import { OpenAPI, useSofa } from "sofa-api";
+
 import { ApolloServer } from "apollo-server-express";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import YAML from "yamljs";
 import client from "./prisma";
 import express from "express";
 import http from "http";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import path from "path";
 import resolvers from "./resolvers";
+import swaggerUi from "swagger-ui-express";
 import typeDefs from "./typeDefs";
+
+const swaggerDocument = YAML.load("./swagger.yml");
 
 async function startApolloServer(port: number) {
   // Required logic for integrating with Express
   const app = express();
   const httpServer = http.createServer(app);
-
   await client.$connect();
   // Same ApolloServer initialization as before, plus the drain plugin.
-  const server = new ApolloServer({
+
+  const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground({})],
   });
 
@@ -30,6 +42,29 @@ async function startApolloServer(port: number) {
     path: "/graphql",
   });
 
+  const openApi = OpenAPI({
+    schema,
+    info: {
+      title: "Open-FI API",
+      version: "1.0.0",
+    },
+  });
+
+  app.use(
+    "/api/v1",
+    useSofa({
+      schema,
+      basePath: "/api/v1",
+      onRoute(info) {
+        openApi.addRoute(info, {
+          basePath: "/api/v1",
+        });
+      },
+    })
+  );
+  openApi.save("./swagger.yml");
+
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   // Modified server startup
   await new Promise<void>((resolve, reject) =>
     httpServer.listen({ port: port }, resolve).once("error", reject)
